d33a37b5b6c80c6a41483dae8f5721029ee542f3
AllisonLin8 <doging8@gmail.com>
2023-05-11 21:05:09 +0800

    Add local login feature via passport

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+}))
+usePassport(app)
 app.use(routes)
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`)
