4a6e4200934ae15db30e4872bc9f0a634af6eb9d
AllisonLin8 <doging8@gmail.com>
2023-05-11 22:31:20 +0800

    Add create todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,6 +3,23 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
+router.get('/new', (req, res) => {
+    return res.render('new')
+})
+router.post('/', (req, res) => {
+    const name = req.body.name
+    const UserId = req.user.id
+    const errors = []
+    if (!name) {
+        errors.push({ message: '請填入待辦事項。' })
+    }
+    if (errors.length) {
+        return res.render('new', { errors })
+    }
+    return Todo.create({ name, UserId })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+})
 router.get('/:id', (req, res) => {
     const id = req.params.id
     const UserId = req.user.id
