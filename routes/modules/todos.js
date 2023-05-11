74aa9ddba7b2ac256d55f1237a9e14408e1fde42
AllisonLin8 <doging8@gmail.com>
2023-05-11 23:49:52 +0800

    Add edit todo feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -27,9 +27,43 @@ router.get('/:id', (req, res) => {
         where: {
             id,
             UserId
-        }
+        },
+        raw: true,
+        nest: true
     })
-        .then(todo => res.render('detail', { todo: todo.toJSON() }))
+        .then(todo => res.render('detail', { todo }))
+        .catch(err => console.log(err))
+})
+router.get('/:id/edit', (req, res) => {
+    const id = req.params.id
+    const UserId = req.user.id
+    return Todo.findOne({
+        where: {
+            id,
+            UserId
+        },
+        raw: true,
+        nest: true
+    })
+        .then(todo => res.render('edit', { todo }))
+        .catch(err => console.log(err))
+})
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const UserId = req.user.id
+    const { name, isDone } = req.body
+    if (!name) {
+        req.flash('warning_msg', '待辦事項欄位為必填。')
+        res.redirect(`/todos/${id}/edit`)
+    }
+    return Todo.update({ name, isDone: isDone === "on" }, {
+        where: {
+            id,
+            UserId
+        }
+    }
+    )
+        .then(() => res.redirect(`/todos/${id}`))
         .catch(err => console.log(err))
 })
 
