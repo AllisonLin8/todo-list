80b58d44e09dca5ea1aa99f9df93fb483cbabbc6
AllisonLin8 <doging8@gmail.com>
2023-05-11 19:56:14 +0800

    Add index page

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
 app.use(routes)
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`)
